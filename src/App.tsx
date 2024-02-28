92ace1d8fd715b8d776f2cd136f77a0463316e17
Marcin Barc <marb637@gmail.com>
2024-02-28 20:02:47 +0100

    mutationFn parameter typed by ExampleData

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import { useQuery } from 'react-query';
+import { useQuery, useMutation } from 'react-query';
 import './App.css';
 
 interface ApiData {
+    id: number;
+    title: string;
+}
+
+interface ExampleData {
     id: number;
     title: string;
 }
@@ -15,10 +20,26 @@ function App() {
             ),
     });
 
-    if (error) return <div>There was an error!</div>;
+    const {
+        mutate,
+        isLoading: isPending,
+        isError,
+    } = useMutation({
+        mutationFn: (newPost: ExampleData) =>
+            fetch('https://jsonplaceholder.typicode.com/posts', {
+                method: 'POST',
+                body: JSON.stringify(newPost),
+            }).then((res) => res.json()),
+    });
+
+    if (error || isError) return <div>There was an error!</div>;
     if (isLoading) return <div>Loading...</div>;
     return (
         <>
+            {isPending && <h1>Data is being added...</h1>}
+            <button onClick={() => mutate({ id: 1, title: 'Do Laundry' })}>
+                Add post
+            </button>
             {data?.map(({ id, title }: ApiData) => {
                 return <div key={id}>{title}</div>;
             })}
